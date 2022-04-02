646844758a68deed465ac99aa2d2da39eb066fe9
florianseksi <florian.fani@hotmail.com>
2022-04-02 13:17:30 +0200

    JavaScript Slider, changes in big pictures

--- Check-In Bar/index.js
+++ Check-In Bar/index.js
@@ -65,3 +65,33 @@ formReg.addEventListener('submit', (e) => {
     }
 
 })
+
+
+
+
+
+
+/*                                     SLIDER                                */ 
+
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+
+//Button Listeners
+nextBtn.addEventListener('click',()=>{
+    carouselSlide.style.transition = "transform 0.5s ease-in-out";
+    counter++;
+    console.log(counter);
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
